0b7dd940f9f52e01020956fdbe92d87d1a69de9e
praattikk <109297179+praattikk@users.noreply.github.com>
2024-03-21 13:50:06 +0530

    Update list-users.sh

--- list-users.sh
+++ list-users.sh
@@ -36,6 +36,13 @@ function list_users_with_read_access {
     fi
 }
 
+function helper {
+ expected_cmd_args=2
+ if [ $# -ne $expected_cmd_args]; then
+        echo "please execute the script with cmd args"
+        echo "asd"
+} 
+
 # Main script
 
 echo "Listing users with read access to ${REPO_OWNER}/${REPO_NAME}..."
